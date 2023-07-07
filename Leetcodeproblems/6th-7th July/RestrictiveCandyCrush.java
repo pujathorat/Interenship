8e8c63b2fbf7f7a9a28a781aa665aee78a614fba
pujathorat <110715736+pujathorat@users.noreply.github.com>
2023-07-07 17:26:53 +0530

    Add files via upload

---
+++ Leetcodeproblems/6th-7th July/RestrictiveCandyCrush.java
@@ -0,0 +1,31 @@
+package com.java.LeetcodeproblemsJuly;
+
+
+import java.util.Stack;
+
+class RestrictiveCandyCrush {
+    public static void reduceString(String str) {
+    	StringBuffer sb = new StringBuffer(str);
+int i=0;
+while(i<sb.length()-1) {
+	
+	char ch1=sb.charAt(i);
+	char ch2=sb.charAt(i+1);
+	if(ch1==ch2) {
+	
+		
+		sb.delete(i, i+2);
+		if(i!=0) 
+			i--;
+		continue;
+	}
+	i++;
+}
+        System.out.println(sb);
+    }
+    public static void main(String[] args) {
+    	System.out.println("Reduced String is:");
+      reduceString("google");
+    }
+}
+
